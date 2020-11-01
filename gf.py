5e289c1bd2cdd0749f0bd76dd8a25ae74008f13d
Noah Hobson <nhobson@vols.utk.edu>
2020-11-01 16:33:17 -0500

    added repl to end of linereg.py

---
+++ gf.py
@@ -0,0 +1,26 @@
+import re as regex
+
+def gunning_fog(line):
+    words = "".join(line.strip().split("/")).split()
+    num_words = len(words)
+    num_sent = len(line.split('.'))
+ 
+    long_words = 0
+    for word in words:
+        letters = regex.compile('[a-zA-Z]')
+        if (letters.search(word) == None):
+            num_words -= 1
+        syllables = regex.compile("[aeiou]+")
+        num_syllables = len(syllables.findall(word)) # this won't be exact, but it's close enough
+        if (word.endswith("ed") or word.endswith("es") or word.endswith("ing")):
+            num_syllables -= 1
+        if (num_syllables >= 3):
+            long_words += 1
+ 
+    if (num_words == 0):
+        return 0
+
+    ASL = num_words / num_sent
+    PHW = 100 * long_words / num_words
+ 
+    return 0.4 * (ASL + PHW)
--- linreg.py
+++ linreg.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import sys
+import gf
 
 if (len(sys.argv) == 1):
     print("Usage: python3 linreg3.py input_file [--squares --cubes]")
@@ -74,27 +75,27 @@ def linreg(expected, features):
     print("\tR² = " + str(r2_score(Y, Yp)))
     # print("\tAverage distance from correct: " + str(np.average(Y-Yp)))
 
-    return Y, Yp
+    return Y, Yp, w
 
 
 print("\nCommit size:")
 features_c = ["num_files_changed", "num_lines_changed"]
 expected_c = "rank_commit_size"
-rank_commit_size, rank_commit_size_p = linreg(expected_c, features_c)
+rank_commit_size, rank_commit_size_p, weights_c = linreg(expected_c, features_c)
 percent_correct_c = np.sum(rank_commit_size == rank_commit_size_p) / len(rank_commit_size)
 print("\tPercent correct: " + str(percent_correct_c))
 
 print("\nMessage length:")
 features_l = ["gunning_fog_approx", "num_chars", "num_words"]
 expected_l = "rank_message_length"
-rank_message_length, rank_message_length_p = linreg(expected_l, features_l)
+rank_message_length, rank_message_length_p, weights_l = linreg(expected_l, features_l)
 percent_correct_l = np.sum(rank_message_length == rank_message_length_p) / len(rank_message_length)
 print("\tPercent correct: " + str(percent_correct_l))
 
 print("\nMessage readability")
 features_r = ["gunning_fog_approx", "num_chars", "num_words"]
 expected_r = "rank_message_readability"
-rank_message_readability, rank_message_readability_p = linreg(expected_r, features_r)
+rank_message_readability, rank_message_readability_p, weights_r = linreg(expected_r, features_r)
 percent_correct_r = np.sum(rank_message_readability == rank_message_readability_p) / len(rank_message_readability)
 print("\tPercent correct: " + str(percent_correct_r))
 
@@ -108,3 +109,23 @@ csv_output = [rank_commit_size, rank_message_length, rank_message_readability,
 csv_output = list(np.array(csv_output).T)
 
 pd.DataFrame(csv_output).to_csv("ranks.csv", index=False, header=False)
+
+while True:
+    try:
+        s = input("Enter commit message: ")
+    except EOFError:
+        print()
+        break
+
+    _gf = gf.gunning_fog(s)
+    num_chars = len(s)
+    num_words = len(s.split())
+    feats = [_gf, num_chars, num_words]
+    if (squares):
+        feats.append(_gf**2)
+        feats.append(num_chars**2)
+        feats.append(num_words**2)
+    feats.append(1)
+
+    test = np.array(feats)
+    print("Readability: " + str(f(weights_r, test)))
